f543be1a18a812c3b2e4e1b990b266c5ebc15af6
REUSS <mreu@dr.com>
2024-07-18 02:39:53 +0700

    Layout class create and basic definitions

---
+++ src/stellargui.lua
@@ -0,0 +1,40 @@
+-- stellar
+local stellar = {}
+
+
+
+-- documentation
+
+---@alias pixels number Amount of pixels.
+---@alias seconds number Amount of seconds
+---@alias DimensionsXYWH {[1]: pixels, [2]: pixels, [3]: pixels, [4]:pixels} Structure for some object's X position, Y position, width and height.
+
+-- config
+
+
+
+-- consts
+
+
+
+-- vars
+
+
+
+-- init
+
+
+
+-- fnc
+
+
+
+-- classes
+
+
+
+-- stellar fnc
+
+
+
+return stellar
