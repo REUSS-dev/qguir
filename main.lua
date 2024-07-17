f0a3f28a98a3a102d60e51111ff864608b94e7e4
REUSS <mreu@dr.com>
2024-07-18 02:40:38 +0700

    New project essentials

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";src/?.lua")
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
---
+++ tmplt.lua
@@ -0,0 +1,38 @@
+-- libname
+local libname = {}
+
+
+
+-- documentation
+
+
+
+-- config
+
+
+
+-- consts
+
+
+
+-- vars
+
+
+
+-- init
+
+
+
+-- fnc
+
+
+
+-- classes
+
+
+
+-- libname fnc
+
+
+
+return libname
