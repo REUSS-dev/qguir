dba1622b11d8a199602195cd5148086acfa99fea
REUSS <mreu@dr.com>
2024-07-24 01:37:03 +0700

    Rename layout to panel

---
+++ src/classes/Panel.lua
@@ -0,0 +1,49 @@
+-- panel
+local panel = {}
+
+local composite = require("classes.CompositeObject")
+
+-- documentation
+
+
+
+-- config
+
+
+
+-- consts
+
+
+
+-- vars
+
+
+
+-- init
+
+
+
+-- fnc
+
+
+
+-- classes
+
+---@class Panel: CompositeObject
+local Panel = {}
+local Panel_meta = {__index = Panel}
+
+setmetatable(Panel, {__index = composite.class}) -- Set parenthesis
+
+-- panel fnc
+
+function panel.new(dimensions)
+    local obj = composite.new(dimensions)
+    ---@cast obj Panel
+
+    setmetatable(obj, Panel_meta)
+
+    return obj
+end
+
+return panel
