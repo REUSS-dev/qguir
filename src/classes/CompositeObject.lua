ea44cb3e004d41ea4c7c50bd763ed351141798f8
REUSS <mreu@dr.com>
2024-07-25 02:22:51 +0700

    individual hl values for composite objects

--- src/classes/CompositeObject.lua
+++ src/classes/CompositeObject.lua
@@ -30,7 +30,10 @@ local uiobj = require("classes.ObjectUI")
 -- classes
 
 ---@class CompositeObject : ObjectUI
----@field objects ObjectUI[]
+---@field objects ObjectUI[] List of UI objects inside the composite object
+---@field hlObject ObjectUI|false UI object, that currently has focus out of all UI objects iside the composite object
+---@field hlObjectBuffer ObjectUI|false UI object, that potentially can get focus after the call of onHover()
+---@field clickObjectBuffer ObjectUI|false UI object, that will recieve clickRelease event call when the composite object receives it
 local CompositeObject = {}
 local CompositeObject_meta = {__index = CompositeObject}
 
@@ -39,13 +42,46 @@ local CompositeObject_meta = {__index = CompositeObject}
 ---@param y pixels Mouse Y position in pixels
 ---@return ObjectUI|false hover Returns object pointer if the mouse if hovering on the object, false otherwise
 function CompositeObject:checkHover(x, y)
+    if not uiobj.class.checkHover(self, x, y) then
+        return false
+    end
+
     local hlObject
 
     for _, uiobject in pairs(self.objects) do
         hlObject = uiobject:checkHover(x, y) or hlObject
     end
 
-    return hlObject
+    if not hlObject then
+        return false
+    end
+
+    self.hlObjectBuffer = hlObject
+
+    return self
+end
+
+---Promote buffered hlObject and trigger its hover-on callback when the composite UI object gains hover focus.
+---@param x pixels Mouse X position in pixels
+---@param y pixels Mouse Y position in pixels
+function CompositeObject:hoverOn(x, y)
+    self.hl = true
+
+    self.hlObject = self.hlObjectBuffer
+    self.hlObjectBuffer = false
+
+    self.hlObject:hoverOn(x, y)
+end
+
+---Clear hlObject and trigger its hover-off callback when the composite UI object loses hover focus.
+---@param x pixels Mouse X position in pixels
+---@param y pixels Mouse Y position in pixels
+function CompositeObject:hoverOff(x, y)
+    self.hl = false
+
+    self.hlObject:hoverOff(x, y)
+
+    self.hlObject = false
 end
 
 ---Tick all UI objects in a composite object.
@@ -63,6 +99,28 @@ function CompositeObject:paint()
     end
 end
 
+---Perform click action on one of UI objects inside the composite object
+---@param x pixels
+---@param y pixels
+---@param but number
+function CompositeObject:click(x, y, but)
+    if self.hlObject then
+        self.clickObjectBuffer = self.hlObject
+        self.hlObject:click(x, y, but)
+    end
+end
+
+---Perform click release action on one of UI objects inside the composite object
+---@param x pixels
+---@param y pixels
+---@param but number
+function CompositeObject:clickRelease(x, y, but)
+    if self.clickObjectBuffer then
+       self.clickObjectBuffer:clickRelease(x, y, but)
+       self.clickObjectBuffer = false
+    end
+end
+
 setmetatable(CompositeObject, {__index = uiobj.class}) -- Set parenthesis
 
 -- composite fnc
@@ -72,6 +130,10 @@ function composite.new(dimensions)
     ---@cast obj CompositeObject
 
     obj.objects = {}
+
+    obj.hlObject = false
+    obj.hlObjectBuffer = false
+    obj.clickObjectBuffer = false
 
     setmetatable(obj, CompositeObject_meta)
 
