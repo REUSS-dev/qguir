c958fd7c5ca4bbfa8b3d14a0e90ee35cc408efcb
REUSS <mreu@dr.com>
2024-09-10 21:36:49 +0700

    Pallette class

---
+++ src/classes/Palette.lua
@@ -0,0 +1,100 @@
+-- palette
+local palette = {}
+
+
+
+-- documentation
+
+---@alias ColorIndex number Number that colors inside the palette are indexed by. Agreement: 1 - main color, 2 - text color, 3 - additional color
+---@alias ColorValue number<0,1> Amount of color in a channel. number<0, 1> for LOVE 11 and higher, number<0, 255> for LOVE 0.10 and lower.
+---@alias ColorTable {[1]: ColorValue, [2]: ColorValue, [3]: ColorValue, [4]: ColorValue} Table, containing RGBA color.
+
+-- config
+
+---@enum ColorNames
+local colorNames = {
+    ---@see ColorIndex 1 - Main color
+    color = 1,
+    fg = 1,
+    foreground = 1,
+    main = 1,
+
+    ---@see ColorIndex 2 - Text color
+    text = 2,
+
+    ---@see ColorIndex 3 - Additional color
+    additional = 3,
+    background = 3,
+    bg = 3,
+    frame = 3,
+    hl = 3,
+}
+
+-- consts
+
+
+
+-- vars
+
+
+
+-- init
+
+
+
+-- fnc
+
+
+
+-- classes
+
+---@class Palette
+---@field container {[ColorIndex]: ColorTable}
+local Palette = {}
+local Palette_meta = {}
+
+function Palette_meta:__index(key)
+    local colorindex = colorNames[key]
+
+    if colorindex then
+        return self.container[colorindex]
+    else
+        return Palette[key]
+    end
+end
+
+---Get color table by color index
+---@param index ColorIndex
+---@return ColorTable
+function Palette:getColorByIndex(index)
+    return self.container[index]
+end
+
+---Set a color in the palette by index or name
+---@param index_or_name ColorNames|ColorIndex
+---@param color ColorTable
+function Palette:setColor(index_or_name, color)
+    if type(index_or_name) == "string" then
+        self.container[colorNames[index_or_name]] = color
+    else
+        self.container[index_or_name] = color
+    end
+end
+
+-- palette fnc
+
+---Create new palette from an array of color tables
+---@param colors ColorTable[]
+---@return Palette PaletteObject
+function palette.new(colors)
+    ---@type Palette
+    local obj = {
+        container = colors or {}
+    }
+
+    setmetatable(obj, Palette_meta)
+
+    return obj
+end
+
+return palette
