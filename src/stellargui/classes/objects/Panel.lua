496857cb4f073dfdd22987526c121a9d89c4fd9e
qq <mreu@dr.com>
2025-05-27 20:28:01 +0700

    Panel: realization

--- src/stellargui/classes/objects/Panel.lua
+++ src/stellargui/classes/objects/Panel.lua
@@ -12,10 +12,11 @@ local uiobj = require("classes.ObjectUI")
 panel.name = "Panel"
 panel.aliases = {}
 panel.rules = {
-    {"sizeRectangular", {0, 0, 100, 50}},
-    {"position", {position = {"center", "center"}}},
+    {"sizeRectangular", {0, 0, love.graphics.getWidth(), love.graphics.getHeight()}},
+    {"position", {position = {0, 0}}},
 
-    {"palette", {color = {0, 0, 0, 0}}},
+    {"palette", {color = {1, 1, 1, 1}}},
+    {{"r", "radius", "rounding", "round"}, "r", nil}
 }
 
 -- consts
@@ -39,16 +40,18 @@ panel.rules = {
 ---@class Panel: ObjectUI
 local Panel = {}
 local Panel_meta = {__index = Panel}
-
 setmetatable(Panel, {__index = uiobj.class}) -- Set parenthesis
+
+function Panel:paint()
+    love.graphics.setColor(self.palette[1])
+    love.graphics.rectangle("fill", 0, 0, self.w, self.h, self.r)
+end
 
 -- panel fnc
 
 function panel.new(prototype)
     local obj = uiobj.new(prototype)
-    ---@cast obj Panel
-
-    setmetatable(obj, Panel_meta)
+    setmetatable(obj, Panel_meta)   ---@cast obj Panel
 
     return obj
 end
