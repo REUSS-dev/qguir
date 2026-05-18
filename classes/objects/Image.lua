3a56de41f7ddfc3306f51b9043d2b330941077a7
AnyUnion <mreu@dr.com>
2026-05-19 02:09:06 +0700

    Image: UI object for love.Image

---
+++ classes/objects/Image.lua
@@ -0,0 +1,144 @@
+-- image
+local image = {}
+
+local uiobj = require("classes.ObjectUI")
+
+-- documentation
+
+---@alias ImageDisplayMode "normal"|"stretch"|"limit"
+---@alias ImageDrawCache {x: integer, y: integer, wscale: number, hscale: number}
+
+-- config
+
+image.name = "Image"
+image.aliases = {}
+image.rules = {
+    {"layout", {w = "fill", h = "fill"}},
+	{"palette", {color = {1, 1, 1, 1}}},
+
+	{{"image", "img", "picture"}, "image", nil},
+	{{"displayMode", "display"}, "display", "normal"},
+
+    {{"r", "radius", "rounding", "round"}, "r", nil}
+}
+
+-- consts
+
+
+
+-- vars
+
+local x, y, w, h, r
+
+-- init
+
+
+
+-- fnc
+
+local function stencil_function()
+	love.graphics.rectangle("fill", x, y, w, h, r)
+end
+
+-- classes
+
+---@class Image : ObjectUI
+---@field r number Radius of round corner
+---@field image love.Image
+---@field display ImageDisplayMode
+---@field drawCache ImageDrawCache
+local Image = {}
+local Image_meta = {__index = Image}
+setmetatable(Image, {__index = uiobj.class}) -- Set parenthesis
+
+function Image:paint()
+    love.graphics.setColor(self.palette[1])
+    
+	if self.r then
+		x, y, r, w, h = self.drawCache.x, self.drawCache.y, self.image:getWidth() * self.drawCache.wscale, self.image:getHeight() * self.drawCache.hscale, self.r
+		love.graphics.stencil(stencil_function, "increment", 0, true)
+
+		local oldStencil = {love.graphics.getStencilTest()}
+
+		love.graphics.setStencilTest("gequal", oldStencil[2] + 1)
+
+		love.graphics.draw(self.image, self.drawCache.x, self.drawCache.y, 0, self.drawCache.wscale, self.drawCache.hscale)
+
+		love.graphics.stencil(stencil_function, "decrement", 0, true)
+		love.graphics.setStencilTest(unpack(oldStencil))
+
+		return
+	end
+
+	love.graphics.draw(self.image, self.drawCache.x, self.drawCache.y, 0, self.drawCache.wscale, self.drawCache.hscale)
+end
+
+function Image:resize(new_w, new_h)
+	uiobj.class.resize(self, new_w, new_h)
+
+	self:calculateDrawParameters()
+end
+
+function Image:calculateDrawParameters()
+	if not self.w or not self.h then
+		return
+	end
+
+	local drawCache = {} ---@type ImageDrawCache
+
+	local img_w, img_h = self.image:getDimensions()
+
+	if self.display == "stretch" then
+		drawCache.x = 0
+		drawCache.y = 0
+		drawCache.wscale = self.w / img_w
+		drawCache.hscale = self.h / img_h
+	elseif self.display == "normal" or self.display == "limit" then
+		local scale
+
+		if self.w / self.h >= img_w / img_h then -- мерить по высоте
+			scale = self.h / img_h
+		else -- мерить по ширине
+			scale = self.w / img_w
+		end
+
+		if self.display == "limit" then
+			scale = math.min(scale, 1)
+		end
+
+		drawCache.x = math.floor((self.w - img_w * scale)/2 + .5)
+		drawCache.y = math.floor((self.h - img_h * scale)/2 + .5)
+		drawCache.wscale = scale
+		drawCache.hscale = scale
+	end
+
+	self.drawCache = drawCache
+end
+
+-- image fnc
+
+function image.new(prototype)
+    local obj = uiobj.new(prototype)
+    setmetatable(obj, Image_meta)
+	---@cast obj Image
+
+	if obj.display ~= "normal" and obj.display ~= "stretch" and obj.display ~= "limit" then
+		error("Display mode of an image can be: normal, stretch, limit. Got: " .. obj.display)
+	end
+
+	if not obj.image then
+		error("Image must be provided for an Image object")
+	end
+
+	if obj.layout.w == "hug" then
+		obj.layout.w = obj.image:getWidth()
+	end
+
+	if obj.layout.h == "hug" then
+		obj.layout.h = obj.image:getHeight()
+	end
+
+    return obj
+end
+
+return image
--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ function love.load()
 
 	DEBUG = true
 
-    local box1 = gui.Button{w = "fill", h = 75}
+    local box1 = gui.Image{w = "fill", h = "fill", image = love.graphics.newImage("test.png"), display = "limit"}
 
 	local box2 = gui.Container{w = "fill", h = "fill"}
 
