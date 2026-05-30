9b001926204072f80824d37d913e254f4adbec90
AnyUnion <mreu@dr.com>
2026-05-30 20:21:15 +0700

    Label: Logic for 'hug' width/height

--- classes/objects/Label.lua
+++ classes/objects/Label.lua
@@ -15,10 +15,9 @@ label.name = "Label"
 label.aliases = {}
 label.rules = {
     {{"text", "label"}, "text", "Label"},
-    {{"align"}, "align", "left"},
     {{"font"}, "font", love.graphics.getFont()},
 
-    {"layout", {w = "hug", h = "hug"}},
+    {"layout", {w = "hug", h = "hug", horizontal = "left"}},
 
     {"palette", {color = {0, 0.5, 0, 0.4}, textColor = {1, 1, 1}}},
 }
@@ -45,7 +44,6 @@ label.rules = {
 ---@field textCache table Set of data for printing button text. WARNING: This should be nullified on label size/text change.
 ---@field text string Button text
 ---@field font love.Font Button text font
----@field align love.AlignMode Text align mode
 local Label = {}
 local Label_meta = {__index = Label}
 setmetatable(Label, {__index = uiobj.class}) -- Set parenthesis
@@ -54,10 +52,42 @@ function Label:paint()
     -- Text
     love.graphics.setColor(self.palette[2])
     love.graphics.setFont(self.font)
-    love.graphics.printf(self.textCache.textVisual, 0, self.textCache.y, self.w, self.align)
+    love.graphics.printf(self.textCache.textVisual, 0, self.textCache.y, self.w, self.layout.horizontal)
 end
 
----Regenerate crucial data for button text printing
+function Label:autolayout(fill_w, fill_h)
+	local ow, oh = uiobj.class.autolayout(self, fill_w, fill_h)
+
+	if not ow or not oh then
+		local max_width, wrapped_lines = self.font:getWrap(self.text, ow or math.huge)
+
+		if self.layout.w == "hug" then
+			ow = max_width
+		end
+
+		if self.layout.h == "hug" then
+			local fontHeight = self.font:getHeight()
+
+			oh = fontHeight * #wrapped_lines
+		end
+
+		if ow and oh then
+			if ow ~= self.w or oh ~= self.h then
+				self:resize(ow, oh)
+			end
+		end
+	end
+
+	return ow, oh
+end
+
+function Label:resize(new_w, new_h)
+	uiobj.class.resize(self, new_w, new_h)
+
+	self:generateTextCache()
+end
+
+---Regenerate crucial data for button text printing (to be rewritten for new layout system)
 ---@package
 function Label:generateTextCache()
     self.textCache = {}
@@ -95,8 +125,6 @@ function label.new(prototype)
     local obj = uiobj.new(prototype)
 
     setmetatable(obj, Label_meta) ---@cast obj Label
-
-    obj:generateTextCache()
 
     return obj
 end
